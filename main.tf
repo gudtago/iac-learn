b0fbbe723f9a567214187f1224fb3dee0aa67594
gudtago <1972612+gudtago@users.noreply.github.com>
2023-07-04 11:00:23 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
